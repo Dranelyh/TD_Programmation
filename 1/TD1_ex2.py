ef3091272ad38af34c6bc623fbbbfb33ed6543e8
Kylian <kylianbdu13@gmail.com>
2024-03-13 16:42:14 +0100

    frr

--- 1/TD1_ex2.py
+++ 1/TD1_ex2.py
@@ -1,3 +1,8 @@
-file = open("mots.sansaccent.txt",'r')
-data=file.read()
+words=[]
+
+file = open("frenchssaccent.dic",'r')
+for line in file:
+    words.append(line[0:len(line)-1])
+
+file.close()
 
