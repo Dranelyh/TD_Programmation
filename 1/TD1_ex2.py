102734b74d791f413f21ae177ed85fea76b6c258
Kylian <kylianbdu13@gmail.com>
2024-03-14 13:16:27 +0100

    ajout de la fonction créant la liste de mots

--- 1/TD1_ex2.py
+++ 1/TD1_ex2.py
@@ -1,8 +1,16 @@
-words=[]
+def word_initialization(words:list,text:str):
+    """
+    fonction that return a list of words with 8 or less letters 
+    from a external sources
+    """
+    file = open(text,'r')
+    for line in file:
+        if len(line[0:len(line)-1])<=8:
+            words.append(line[0:len(line)-1])
 
-file = open("frenchssaccent.dic",'r')
-for line in file:
-    words.append(line[0:len(line)-1])
+    file.close()
+    return(words)
 
-file.close()
 
+
+        
