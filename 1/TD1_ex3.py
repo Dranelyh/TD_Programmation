3c4d3d61ccc64f686a1b0773db8bed6911512e04
Kylian <kylianbdu13@gmail.com>
2024-03-14 18:03:32 +0100

    resolution de l'exercice 3

--- 1/TD1_ex3.py
+++ 1/TD1_ex3.py
@@ -1,3 +1,5 @@
+#fonctions
+###############################################################################
 def word_initialization(text:str):
     """
     fonction that return a list of words with 8 or less letters 
@@ -25,7 +27,25 @@ def possible_word(word:str,letters:list):
             letters.pop(letters.index(word_letter[i]))
     return(True)
 
-def longest_word(words:list,letters:list):
+def score(word:str):
+    points={'a':1,'b':3,'c':3,'d':2,'e':1,'f':4,'g':2,'h':4,'i':1,'j':8,'k':10,
+            'l':1,'m':2,'n':1,'o':1,'p':3,'q':8,'r':1,'s':1,'t':1,'u':1,'v':4,
+            'w':10,'x':10,'y':10,'z':10}
+    letter=list(word)
+    pts=0
+    for i in range(len(letter)):
+        pts+=points[letter[i]]
+    return(pts)
+
+def max_score(words:list):
+    number,pts=0,score(words[0])
+    for i in range(1,len(words)):
+        if score(words[i])>pts:
+            number,pts=i,score(words[i])
+    return(words[number],pts)
+
+
+def best_word(words:list,letters:list):
     """
     Parameters
     ----------
@@ -34,26 +54,21 @@ def longest_word(words:list,letters:list):
     letters : list of string
         letters available
         
-    Returns the longest words with the available letters.
+    Returns the words that give the maximum of points.
     -------
     """
-    longest=''
+    available_words=[]
     for i in range(len(words)):
         copy_letters=letters.copy()
-        if len(longest)==8:
-            return(longest)
-        elif possible_word(words[i], copy_letters) and len(longest)<len(words[i]):
-            longest=words[i]
-    return(longest)
-            
-    
-    
-    
-    
+        if possible_word(words[i], copy_letters):
+            available_words.append(words[i])
+    return(max_score(available_words))
 ###############################################################################
 
 words=word_initialization("mots.sansaccent.txt")
-letters=['o','s','o','u','s','i','n','r']
-longest=longest_word(words, letters)
-print(longest)
+letters=['o','s','a','u','s','n','g','w']
+print(best_word(words,letters))
+
+
+
 
